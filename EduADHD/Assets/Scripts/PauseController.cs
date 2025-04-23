4a3c7af80f7f3ac1b95aec451768c5dd9d255d25
ievaglavecka <ieva.glavecka@valmiera.edu.lv>
2025-04-24 01:32:55 +0300

    started pause menu
    Almost fully implemented pause menu during gameplay

---
+++ EduADHD/Assets/Scripts/PauseController.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+
+
+public class PauseController : MonoBehaviour
+{
+    bool gamePaused = false;
+    public GameObject pauseMenu;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && gamePaused == false)
+        {
+            PauseGame();
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape) && gamePaused == true)
+        {
+            ResumeGame();
+        }
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+        gamePaused = false;
+        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    public void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0;
+        gamePaused = true;
+        
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    public void Home()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Continue()
+    {
+        Time.timeScale = 1;
+        gamePaused = false;
+        pauseMenu.SetActive(false);
+    }
+}
